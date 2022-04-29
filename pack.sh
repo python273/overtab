b6098fbb08dad3c1cfef8a3221b330d55505a1af
python273 <iam@python273.pw>
2022-04-29 14:41:01 +0300

    Update version to 2.0

---
+++ pack.sh
@@ -0,0 +1,2 @@
+rm overtab.zip
+zip overtab.zip background.js icon.png manifest.json overtab.html page.js
--- page.js
+++ page.js
@@ -101,16 +101,22 @@ const createTabEl = (tab) => {
     // if (tab.active) tabEl.classList.add("tabActive");
     // if (tab.highlighted) tabEl.classList.add("tabHighlighted");
 
+    const tabIconContainerEl = document.createElement('div');
+    tabIconContainerEl.classList.add("tabIconContainer");
+    tabEl.appendChild(tabIconContainerEl);
+
     if (tab.favIconUrl) {
         const tabIcon = document.createElement('img');
         tabIcon.classList.add("tabIcon");
         tabIcon.src = tab.favIconUrl;
-        tabEl.appendChild(tabIcon);
+        tabIconContainerEl.appendChild(tabIcon);
     }
 
     const tabTitle = document.createElement('span');
     tabTitle.classList.add("tabTitle");
     tabTitle.innerText = tab.title;
+    // tabTitle.innerText = new Date(tab.lastAccessed).toLocaleString();
+
     tabEl.appendChild(tabTitle);
 
     tabEl.addEventListener('click', onTabClick);
@@ -133,8 +139,8 @@ browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 
 browser.tabs.onRemoved.addListener((tabId) => {
     const selectorsToRemove = [
-        '#' + tabElId(tabId),
-        '#' + `tab_popover_${tabId}`,
+        `#${tabElId(tabId)}`,
+        `#tab_popover_${tabId}`,
     ];
     for (const s of selectorsToRemove) {
         const els = document.querySelectorAll(s);
@@ -145,6 +151,7 @@ browser.tabs.onRemoved.addListener((tabId) => {
 })
 
 async function _renderTabs() {
+    const currentWindowId = (await browser.windows.getCurrent()).id;
     let tabs = await browser.tabs.query({});
 
     if (query) {
@@ -175,7 +182,7 @@ async function _renderTabs() {
     if (sortByUrl) {
         tabs.sort((a, b) => a.url.localeCompare(b.url));
     } else {
-        tabs.sort((a, b) => a.index - b.index);
+        tabs.sort((a, b) => b.index - a.index);  // reverse
     }
 
     const tabsByWindows = {};
@@ -196,18 +203,50 @@ async function _renderTabs() {
     const windowsEl = document.createElement('div');
     windowsEl.id = "windows";
 
-    for(let windowId in tabsByWindows) {
+    // sort windows, first current window, then others
+    const windowIds = Object.keys(tabsByWindows).sort((a, b) => {
+        if (a == currentWindowId) return -1;
+        if (b == currentWindowId) return 1;
+        return 0;
+    });
+
+    for(let windowId of windowIds) {
         const windowTabs = tabsByWindows[windowId];
+        if (!windowTabs.length) {
+            continue;
+        }
 
         const windowEl = document.createElement('div');
         windowEl.classList.add("window");
         windowsEl.appendChild(windowEl);
 
+        let subgroupEl = document.createElement('div');
+        subgroupEl.classList.add("subgroup");
+        windowEl.appendChild(subgroupEl);
+
+        let privTime = windowTabs[0].lastAccessed;
+
         for (let tab of windowTabs) {
+            if (separateByTime && (Math.abs(tab.lastAccessed - privTime) > 4 * 60 * 60 * 1000)) {
+                const separatorEl = document.createElement('div');
+                separatorEl.classList.add("separator");
+                separatorEl.innerText = new Date(tab.lastAccessed).toLocaleString();
+                windowEl.appendChild(separatorEl);
+
+                subgroupEl = document.createElement('div');
+                subgroupEl.classList.add("subgroup");
+                windowEl.appendChild(subgroupEl);
+            }
+
+            privTime = tab.lastAccessed;
             const tabEl = createTabEl(tab);
-            windowEl.appendChild(tabEl);
+            subgroupEl.appendChild(tabEl);
         }
     }
+
+    const scrollEl = document.createElement('div');
+    scrollEl.classList.add("extra-scroll");
+    windowsEl.appendChild(scrollEl);
 
     rootEl.replaceChildren(windowsEl);
 }
@@ -272,6 +311,17 @@ function onQueryIsRegexChange(event) {
 
 document.getElementById('queryIsRegex').addEventListener('change', onQueryIsRegexChange);
 document.getElementById('queryIsRegex').checked = false;
+
+let separateByTime = false;
+
+function onSeparateByTimeChange(event) {
+    closeAllTabPopovers();
+    separateByTime = event.target.checked;
+    renderTabs().then(() => {});
+}
+
+document.getElementById('separateByTime').addEventListener('change', onSeparateByTimeChange);
+document.getElementById('separateByTime').checked = false;
 
 // function onKeyupHotkey(event) {
 //     console.log(event);
